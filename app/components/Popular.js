8d35a798e694a352a2505bebb04e0957e616e078
Michael Hicks <kustomdesigner@gmail.com>
2021-12-13 08:41:44 -0600

    Fetching repositories

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,19 +1,92 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { fetchPopularRepos } from './fetchPopularRepos'
 
-class Popular extends React.Component {
-  render() {
-    const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
+
+function LanguagesNav({ selected, onUpdateLanguage }) {
+  const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
     
     return (
       <ul className='flex-center'>
         {languages.map((language) => (
           <li key={language}>
-            <button className='btn-clear nav-link'>
+            <button
+              className='btn-clear nav-link langbtn'
+              style={language === selected ? { background:'#eee', color: 'rgb(187, 46, 31)'} : null}
+              onClick={() => onUpdateLanguage(language)}
+            >
               {language}
             </button>
           </li>
         ))}
       </ul>
+    )
+}
+
+LanguagesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  onUpdateLanguage: PropTypes.func.isRequired
+}
+
+
+class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedLanguage: 'All',
+      repos: null,
+      error: null
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this)
+    this.isLoading = this.isLoading.bind(this)
+  }
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage)
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({
+      selectedLanguage,
+      error: null,
+      repos: null
+    })
+
+    fetchPopularRepos(selectedLanguage)
+      .then((repos) => this.setState({
+        repos,
+        error: null
+      }))
+      .catch((err) => {
+        console.warn('Error fetching repos: ', err)
+
+        this.setState({
+          error: `There was an error fetching the respositories`
+        })
+      })
+    }
+    
+    isLoading() {
+      return this.state.repos === null && this.state.error === null
+    }
+  render() {
+    const { selectedLanguage, repos, error } = this.state
+
+    return (
+      <React.Fragment>
+        <LanguagesNav 
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
+        />
+
+        {this.isLoading() && <p>LOADING</p>}
+
+        {error && <p>{error}</p>}
+
+        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+      </React.Fragment>
     )
   }
 }
--- app/index.js
+++ app/index.js
@@ -4,6 +4,22 @@ import './index.css'
 import Popular from './components/Popular'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: 'Mike'
+    }
+
+    this.updateName = this.updateName.bind(this)
+  }
+
+  updateName() {
+    this.setState({
+      name: "Michael",
+    });
+  }
+
   render() {
     return (
       <div className='container'>
